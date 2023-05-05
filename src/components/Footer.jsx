99fcbd9cdb75b466286a6348b5f0fc0fa84ab691
ratchaphon1412 <ratchaphon.h111@gmail.com>
2023-05-05 09:00:20 +0700

    fix detail footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,11 +3,11 @@ import React from 'react'
 function Footer(){
     return(
       
-        <footer className="bg-white rounded-lg shadow m-4 dark:bg-gray-800">
-            <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
+        <footer className="bg-white rounded-lg shadow m-4    dark:bg-gray-800">
+            <div className="w-full mx-auto max-w-screen-xl p-4 md:flex items-center justify-center">
             <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a href="https://flowbite.com/" className="hover:underline">Flowbite™</a>. All Rights Reserved.
             </span>
-            <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
+            {/* <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
                 <li>
                     <a href="#" className="mr-4 hover:underline md:mr-6 ">About</a>
                 </li>
@@ -20,7 +20,7 @@ function Footer(){
                 <li>
                     <a href="#" className="hover:underline">Contact</a>
                 </li>
-            </ul>
+            </ul> */}
             </div>
         </footer>
     )
