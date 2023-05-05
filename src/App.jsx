a30d5437ae6060226433f912cd54cda802fe7cd0
ratchaphon1412 <ratchaphon.h111@gmail.com>
2023-05-05 09:00:56 +0700

    add Landing to Pages App

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Base  from './layouts/base'
+import LandingPage from './pages/LandingPage'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,7 +11,7 @@ function App() {
   return (
     <>
     <Base>
-    Hello
+    <LandingPage/>
     </Base>
 
 
