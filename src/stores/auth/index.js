10ac1478941220b60f06442430895796450202d7
ratchaphon1412 <ratchaphon.h111@gmail.com>
2023-05-05 08:49:24 +0700

    create function async login to send email and password to backend and get token with redux

--- src/stores/auth/index.js
+++ src/stores/auth/index.js
@@ -1,28 +1,60 @@
-import { createSlice } from '@reduxjs/toolkit'
-// import {axiosInstance} from '../../utils/api/axiosIntance';
+import { createSlice,createAsyncThunk } from '@reduxjs/toolkit'
+
+import axiosInstance from '../../utils/api/axiosIntance.js';
+
+export const login = createAsyncThunk('auth/login', async ({ email, password }) => {
+    const response = await axiosInstance.post('/api/signin', { email, password });
+    const data = await response.data;
+    console.log(data)
+    return data;
+  });
+  
+
 
 const initialStateAuth = {
     isAuth: false,
+    user:null,
     email:'',
     password:'',
-    token: '',
+    token: null,
 }
 
 export  const  authSlice = createSlice({name:'auth', initialState: initialStateAuth, 
 reducers: {
-    login: (state, action) => {
-        const { email, password } = action.payload;
-        state.email = email;
-        state.password = password;
-        const response = axiosInstance.post("/login", {email:email, password:password});
+    auth:(state)=>{
+        if(localStorage.getItem('token')){
+            state.isAuth = true;
+        }else{
+            state.isAuth = false;
+        }
 
-        
 
+    },
+
+    logout: (state) => {
+        localStorage.removeItem('token');
     }
 
 
-}
+},
+extraReducers:(builder) => {
+    builder
+      .addCase(login.pending, (state) => {
+        state.status = 'loading';
+      })
+      .addCase(login.fulfilled, (state, action) => {
+        state.status = 'succeeded';
+        state.email = action.payload.email;
+        state.token = action.payload.token;
+        localStorage.setItem('token', action.payload.token);
+       
+      })
+      .addCase(login.rejected, (state, action) => {
+        state.status = 'failed';
+        state.error = action.error.message;
+      });
+  },
 });
 
-export const {login} = authSlice.actions;
+export const {auth,logout} = authSlice.actions;
 export default authSlice.reducer;
