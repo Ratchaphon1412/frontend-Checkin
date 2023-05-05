01ac453756c276ccad9d54b3c5da623dcb47de22
ratchaphon1412 <ratchaphon.h111@gmail.com>
2023-05-05 19:16:28 +0700

    add image Dashboard to assets

---
+++ src/pages/Point.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Base from '../layouts/base'
+function  Point(){
+
+    return ( 
+        <Base>
+           <div className="mx-auto w-full h-screen mt-32">
+            <div className="flex flex-col justify-center items-center">
+                <h1 className="text-black font-bold">Your stars 30</h1>
+            </div>
+        </div>
+        </Base>
+
+    )
+
+}
+
+export default Point;
