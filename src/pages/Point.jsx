0eb2804df7e02730500c9e3cd57e18a23cad8034
ratchaphon1412 <ratchaphon.h111@gmail.com>
2023-05-06 13:17:42 +0700

    add component exchange to Point Page

--- src/pages/Point.jsx
+++ src/pages/Point.jsx
@@ -1,14 +1,34 @@
 import React from 'react'
 import Base from '../layouts/base'
+import CardReward from '../components/CardReward';
 function  Point(){
 
     return ( 
         <Base>
-           <div className="mx-auto w-full h-screen mt-32">
+           <div className="mx-auto w-full h-full mt-32">
             <div className="flex flex-col justify-center items-center">
-                <h1 className="text-black font-bold">Your stars 30</h1>
+                <h1 className="text-black font-bold mb-4">Your stars 30</h1>
+                <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-xl">Consign</button>
             </div>
-        </div>
+            <div className="grid grid-cols-6 mt-8">
+           
+                <div className="col-start-2 col-span-4 justify-center items-center">
+                    <div class="grid md:grid-cols-6   grid-cols-4">
+                       <div class="col-start-2 col-span-2">
+                       <h1 className="text-lg font-bold ">Reward Exchange</h1>
+                       </div>
+                    </div>
+                    <div class="mt-3 flex flex-col justify-center items-center gap-4">
+                    
+                        <CardReward/>
+                        <CardReward/>
+                        <CardReward/>
+                    </div>
+                </div>
+                   
+               
+            </div>
+            </div>
         </Base>
 
     )
