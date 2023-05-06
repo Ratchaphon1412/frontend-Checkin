5cd7e67059bb332ba8c71c215a3fe044f171cd87
ratchaphon1412 <ratchaphon.h111@gmail.com>
2023-05-06 13:19:06 +0700

    add function substraction day form array

--- src/pages/Calendar.jsx
+++ src/pages/Calendar.jsx
@@ -1,1 +1,61 @@
 import React from 'react'
+import EmployeeCalendar from '../components/EmployeeCalendar.jsx';
+import Base from '../layouts/Base.jsx'
+import moment from 'moment';
+
+function CalendarPage() {
+    const markedDates = [
+        "2023-05-05",
+        "2023-05-10",
+        "2023-05-15",
+      ];
+
+      function formatDate(markedDates){
+        markedDates.forEach((date, index) => {
+            let newDate = moment(date).subtract(1, "days");
+            markedDates[index] = newDate.format('YYYY-MM-DD');
+        }   
+      )
+      console.log(markedDates)
+      return markedDates;
+    }
+    formatDate(markedDates)
+
+    return (
+        <Base>
+        <div className="w-full h-screen">
+        <div className="flex flex-col justify-center items-center">
+            <div className="flex flex-col justify-center items-center mb-4 ">
+                    <h1 className="text-xl  mb-4">Check-in</h1>
+               <div className="flex flex-row  justify-center items-center ">
+                   
+                   <h1 className="text-xl mr-6">Check-in for event:</h1>
+                    <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-xl">Click</button>
+                   
+               </div>
+       
+                
+            </div>
+            
+            <div  className='flex flex-col justify-start'>
+            <h1 className="text-xl mr-6 mb-4">Check-in for daily</h1>  
+            <EmployeeCalendar markedDates={markedDates} />
+            </div>
+
+            <div className="h-96 flex flex-col justify-center items-center">
+            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-xl">Click-in-today</button>
+            </div>
+        </div>
+
+        
+
+
+        </div>
+        </Base>
+
+
+    )
+
+}
+
+export default CalendarPage;
