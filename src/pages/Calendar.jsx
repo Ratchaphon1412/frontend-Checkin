55ba385d39015e65b6b4dff6b05adf40ff8c47d2
ratchaphon1412 <ratchaphon.h111@gmail.com>
2023-05-08 02:30:19 +0700

    wrong path

--- src/pages/Calendar.jsx
+++ src/pages/Calendar.jsx
@@ -1,6 +1,6 @@
 import React,{useState,useEffect} from 'react'
 import EmployeeCalendar from '@/components/EmployeeCalendar.jsx';
-import Base from '@/layouts/Base.jsx'
+import Base from '@/layouts/base.jsx'
 import moment from 'moment';
 import  {useDispatch,useSelector} from 'react-redux'
 import {getHistory} from '@/stores/api/index'
