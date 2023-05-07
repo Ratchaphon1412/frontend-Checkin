3b2dbaa2e20ab54b45932a450e802f0e15057a63
ratchaphon1412 <ratchaphon.h111@gmail.com>
2023-05-08 01:38:35 +0700

    create store api

---
+++ src/stores/api/index.js
@@ -0,0 +1,86 @@
+import { createSlice,createAsyncThunk } from '@reduxjs/toolkit'
+
+import axiosInstance from '../../utils/api/axiosIntance.js'
+
+
+export const useConsign = createAsyncThunk('api/consign', async ({ emailFrom, emailTo,starConsign,text }) => {
+    console.log(emailFrom, emailTo,starConsign,text)
+    const response = await axiosInstance.post('api/consign', { emailFrom, emailTo,starConsign,text });
+    const data = await response.data;
+   
+    return data;
+  });
+
+ export const getCoin = createAsyncThunk('api/getCoin', async () => {
+    const response = await axiosInstance.get('api/userConsign');
+    const data = await response.data;
+ 
+    return data;
+ }); 
+
+export const getHistory = createAsyncThunk('api/userDateCheckIn', async () => {
+  const  response = await axiosInstance.get('api/userDateCheckIn');
+  const  data = await response.data;
+  
+  return data;
+
+});
+
+  let initialStateAPI = {
+    coin:0,
+    code:500,
+    date:[],
+
+    }
+
+export  const  apiSlice = createSlice({name:'api', initialState: initialStateAPI, 
+    reducers: {
+      
+    
+    
+    },
+    extraReducers:(builder) => {
+        builder
+          .addCase(useConsign.pending, (state) => {
+            state.status = 'loading';
+          })
+          .addCase(useConsign.fulfilled, (state, action) => {
+            state.status = 'succeeded';
+            state.code = action.payload.code;
+           
+          })
+          .addCase(useConsign.rejected, (state, action) => {
+            state.status = 'failed';
+            state.error = action.error.message;
+          })
+          .addCase(getCoin.pending, (state) => {
+            state.status = 'loading';
+          })
+          .addCase(getCoin.fulfilled, (state, action) => {
+            state.status = 'succeeded';
+            state.coin = action.payload.points;
+           
+          })
+          .addCase(getCoin.rejected, (state, action) => {
+            state.status = 'failed';
+            state.error = action.error.message;
+          })
+          .addCase(getHistory.pending, (state) => {
+            state.status = 'loading';
+          } )
+          .addCase(getHistory.fulfilled, (state, action) => {
+            state.status = 'succeeded';
+            state.date = action.payload.dateHistory;
+           
+           
+           
+          } )
+          .addCase(getHistory.rejected, (state, action) => {
+            state.status = 'failed';
+            state.error = action.error.message;
+          } )
+
+      },
+    });
+
+export default apiSlice.reducer;
