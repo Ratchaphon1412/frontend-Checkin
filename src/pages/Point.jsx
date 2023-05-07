3c6fd1f20d8212948718a905d5be4864d7c2485c
ratchaphon1412 <ratchaphon.h111@gmail.com>
2023-05-08 01:27:00 +0700

    get coin from  api to point  page

--- src/pages/Point.jsx
+++ src/pages/Point.jsx
@@ -1,14 +1,20 @@
-import React from 'react'
+import React,{useEffect} from 'react'
 import Base from '../layouts/base'
 import CardReward from '../components/CardReward';
 import {Link, useNavigate} from 'react-router-dom'
+import {getCoin} from '../stores/api/index'
+import {useSelector,useDispatch} from 'react-redux'
 function  Point(){
-
+    const dispatch = useDispatch();
+    useEffect(() => {
+        dispatch(getCoin())
+    }, [])
+    let coinUser = useSelector(state => state.apiStore.coin)
     return ( 
         <Base>
            <div className="mx-auto w-full h-full mt-32">
             <div className="flex flex-col justify-center items-center">
-                <h1 className="text-black font-bold mb-4">Your stars 30</h1>
+                <h1 className="text-black font-bold mb-4">Your stars {coinUser}</h1>
                 <Link to={`/consign`} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-xl">Consign</Link>
             </div>
             <div className="grid grid-cols-6 mt-8">
