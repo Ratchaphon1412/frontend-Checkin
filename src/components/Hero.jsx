e046e20333f49a1dfa430d14085566d8697b8ca3
ratchaphon1412 <ratchaphon.h111@gmail.com>
2023-05-09 00:23:00 +0700

    landing page link to login

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,6 @@
 import Container from "./Container";
 import heroImg from "@/assets/img/hero.png";
+import {Link} from "react-router-dom";
 
 const Hero = () => {
   return (
@@ -15,13 +16,12 @@ const Hero = () => {
             </p>
 
             <div className="flex flex-col items-start space-y-3 sm:space-x-4 sm:space-y-0 sm:items-center sm:flex-row">
-              <a
-                href="https://web3templates.com/templates/nextly-landing-page-template-for-startups"
-                target="_blank"
+              <Link to={`/login`}
+              
                 rel="noopener"
                 className="px-8 py-4 text-lg font-medium text-center text-white bg-indigo-600 rounded-md ">
                 Login
-              </a>
+              </Link>
               <a
                 href="https://github.com/web3templates/nextly-template/"
                 target="_blank"
