f06a025ee3b86e2e1c7c5e2e05bc9446e6f6edf1
ratchaphon1412 <ratchaphon.h111@gmail.com>
2023-05-09 00:06:51 +0700

    create popup when confirm form

--- src/components/FormConsign.jsx
+++ src/components/FormConsign.jsx
@@ -3,6 +3,8 @@ import {useSelector,useDispatch} from 'react-redux'
 import { useConsign} from '@/stores/api/index'
 import {setEmail} from '@/stores/auth/index'
 import {useNavigate} from 'react-router-dom'
+import classNames from 'classnames';
+import '@/assets/css/popup.css'
 
 
 
@@ -13,19 +15,38 @@ function FormConsign(){
     const [toggle,setToggle] = useState(false);
     const [text ,setText] = useState('');
     const navigate = useNavigate();
+    const email = useSelector(state => state.authStore.email);
+    const [emailUser,setEmailUser] = useState();
+
+    const popupShow  = classNames('fixed  top-0  left-0  right-0 bottom-0 flex flex-row justify-center items-center bg-black bg-opacity-30 z-50',{
+        'hidden':!toggle,
+    });
 
     
     const dispatch = useDispatch();
     useEffect(() => {   
-        dispatch(setEmail(emailUser))
-    }, [])
-    const [emailUser,setEmailUser] = useState(useSelector(state => state.authStore.email));
+        dispatch(setEmail())
+        
+        
+    }, [dispatch])
+
+   useEffect(() => {
+    setEmailUser(email)
+   }, [email])
     
     
     const handleToggle = ()=>{
         setToggle(!toggle);
     }
 
+    useEffect(() => {
+        if(toggle){
+            
+        }
+        else{
+
+        }
+    }, [toggle])
    
     
     const handleChangeEmailTarget = (e)=>{
@@ -80,24 +101,24 @@ function FormConsign(){
 
 
            
-            <button data-modal-target="popup-modal" data-modal-toggle="popup-modal" className="block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button" onClick={handleToggle}>
+            <button  className="block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button" onClick={handleToggle}>
             Submit
             </button>
            
-            <div   tabIndex="-1" className="fixed top-0 left-0 right-0 z-50 hidden p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full">
+            <div   tabIndex="-1" className={popupShow}>
                <div className="relative w-full max-w-md max-h-full">
                    <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
-                       <button type="button" className="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-800 dark:hover:text-white" data-modal-hide="popup-modal">
-                           <svg aria-hidden="true" className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
+                       <button type="button" className="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-800 dark:hover:text-white" onClick={handleToggle}>
+                           <svg aria-hidden="true" className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd" ></path></svg>
                            <span className="sr-only">Close modal</span>
                        </button>
                        <div className="p-6 text-center">
                            <svg aria-hidden="true" className="mx-auto mb-4 text-gray-400 w-14 h-14 dark:text-gray-200" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 8v4m0 4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
                            <h3 className="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">Are you Sure to  Consign?</h3>
-                           <button data-modal-hide="popup-modal" type="button" className="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center mr-2" onClick={handleSubmit}>
+                           <button  type="button" className="text-white bg-green-600 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center mr-2" onClick={handleSubmit}>
                                Yes, I'm sure
                            </button>
-                           <button data-modal-hide="popup-modal" type="button" className="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600" onClick={handleToggle}>No, cancel</button>
+                           <button  type="button" className="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600" onClick={handleToggle}>No, cancel</button>
                        </div>
                    </div>
                </div>
